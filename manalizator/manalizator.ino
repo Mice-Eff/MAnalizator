ad885142988d440c76548fd66098bfe39c7debbe
Mice-Eff <55363328+Mice-Eff@users.noreply.github.com>
2019-11-11 23:25:44 +0300

    Add files via upload

---
+++ manalizator/manalizator.ino
@@ -0,0 +1,47 @@
+ // Powered by Mice_Eff          
+            #include <Adafruit_NeoPixel.h> //https://github.com/adafruit/Adafruit_NeoPixel
+            #include <SPI.h> 
+            #include <ESP8266WiFi.h> // https://github.com/blynkkk/blynk-library/releases/tag/v0.6.1
+            #include <BlynkSimpleEsp8266.h>
+            #include <SimpleTimer.h>    
+            #include <NewPing.h> // https://github.com/e-Gizmo/NewPing
+#define BLYNK_PRINT Serial           
+#define PIN_TRIG 14// пин триг дальномера
+#define PIN_ECHO 12// пин эхо дальномера
+#define MAX_DISTANCE 200// макс расстояние измеряемое дальномером
+char auth[] = "GnoMFEVL6YydXU3Lx6ZBqGJjubvNz9-w";// название токена
+char ssid[] = "mice_eff";  //название wi-fi
+char pass[] = "12345677";  //пароль от wi-fi
+  int lo = 72; // длина до дна мусорки
+  int lm = 40; // длина мусорки
+  NewPing sonar(PIN_TRIG, PIN_ECHO, MAX_DISTANCE);
+  SimpleTimer timer;
+
+
+
+void setup(){
+  Serial.begin(9600); 
+  Blynk.begin(auth, ssid, pass);
+  pinMode(PIN_TRIG, OUTPUT); 
+  pinMode(PIN_ECHO, INPUT);
+}
+
+
+void loop(){
+  
+  Blynk.run(); 
+  timer.run(); 
+     unsigned int y = sonar.ping_cm();
+     Blynk.virtualWrite(V7, y); // выводим расстояние на 7 виртуальный пин на виртуальный дисплей
+     int b =(lo-y)*100/lm;
+     if ( b>-1 && b<101  ){
+     Blynk.virtualWrite(V8, b); // выводим проценты на 8 виртуальный пин на виртуальный дисплей
+     if ( b>=95 ){
+     Blynk.notify("Вынеси мусор, ленивая ж***"); // текст уведомления
+      }
+     } else {
+       Blynk.virtualWrite(V8, 100);
+      }
+     }
+    
+
